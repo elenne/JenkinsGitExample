eb8edc0bacf2f60740b291ab3d47326c51f590d1
Alena_Asadchaya <elenne@gmail.com>
2020-12-24 14:12:20 +0300

    second  commit

--- src/main/java/com/epam/esm/JenkinsGitExample.java
+++ src/main/java/com/epam/esm/JenkinsGitExample.java
@@ -3,5 +3,6 @@ package com.epam.esm;
 public class JenkinsGitExample {
     public static void main(String[] args) {
         System.out.println("hello jenkins git");
+        System.out.println("hello jenkins git again");
     }
 }
