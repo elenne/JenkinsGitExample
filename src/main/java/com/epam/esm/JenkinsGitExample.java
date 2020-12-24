39d1d0bdff9bf5112b2947c9b75195287c8e0400
Alena_Asadchaya <elenne@gmail.com>
2020-12-24 16:21:07 +0300

    third  commit

--- src/main/java/com/epam/esm/JenkinsGitExample.java
+++ src/main/java/com/epam/esm/JenkinsGitExample.java
@@ -4,5 +4,6 @@ public class JenkinsGitExample {
     public static void main(String[] args) {
         System.out.println("hello jenkins git");
         System.out.println("hello jenkins git again");
+        System.out.println("hello jenkins git again 2");
     }
 }
